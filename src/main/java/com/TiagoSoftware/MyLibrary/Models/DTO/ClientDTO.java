38e48b18d39f62efea7f29e20ed61078111771ce
Tiago18555 <tcajueiro_1995@hotmail.com>
2023-05-04 17:05:51 -0300

    setting cpf length size to minimum 11

--- src/main/java/com/TiagoSoftware/MyLibrary/Models/DTO/ClientDTO.java
+++ src/main/java/com/TiagoSoftware/MyLibrary/Models/DTO/ClientDTO.java
@@ -15,7 +15,7 @@ public class ClientDTO {
     @Size(min = 3, max = 40)
     public String name;
 
-    @Size(min = 14, max = 16)
+    @Size(min = 11, max = 16)
     @NotBlank
     public String cpf;
 }
